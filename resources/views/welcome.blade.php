4b0f80aa84efc769300ee542d5b8321935e9f416
Josias <josiastech@gmail.com>
2017-04-10 20:18:45 -0600

    UPDATE: control structure in a view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,17 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <h1>Some Content</h1>
-            <p>{{ 2 == 3 ? "Hello": "Dont equal" }}</p>
+            <h1>Control Structures</h1>
+            @if(true)
+                <p>this is only displays if it true</p>
+            @else
+                <p>this only display if is false</p>
+            @endif
+
+            <hr>
+            @for($i = 0; $i< 5; $i++)
+                <p>{{ $i + 1 }}. Iteration</p>
+            @endfor
         </div>
     </div>
 
