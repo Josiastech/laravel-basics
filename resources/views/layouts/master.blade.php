c586951189ebf106bd02e03233364facaae4479e
Josias <josiastech@gmail.com>
2017-04-10 20:51:00 -0600

    UPDATE: use facades to render the CSS path and import fonts

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,9 +6,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Laravel</title>
+    <link href="https://fonts.googleapis.com/css?family=Arizonia" rel="stylesheet">
+git
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
+    <!-- So-cal Facade -->
+    <link rel="stylesheet" href="{{ URL::to('css/styles.css') }}">
 
 
 </head>
